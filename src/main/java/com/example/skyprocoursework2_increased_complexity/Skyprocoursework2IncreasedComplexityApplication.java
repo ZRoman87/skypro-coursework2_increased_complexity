fdcdfcd744c9cc3e5df4aeacb9ac10f2c53b9936
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-05-31 11:45:44 +0300

    1st commit

---
+++ src/main/java/com/example/skyprocoursework2_increased_complexity/Skyprocoursework2IncreasedComplexityApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprocoursework2_increased_complexity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skyprocoursework2IncreasedComplexityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skyprocoursework2IncreasedComplexityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skyprocoursework2_increased_complexity/Skyprocoursework2IncreasedComplexityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprocoursework2_increased_complexity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skyprocoursework2IncreasedComplexityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
